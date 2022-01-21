4026a3626d67b4c613f42f3d5f3ceafe41cab06f
maximilian-graf2019 <maxgraf27@gmail.com>
2022-01-21 17:07:44 +0100

    testing

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ import { parse } from 'https://deno.land/std/flags/mod.ts';
 const { args } = Deno;
 const DEFAULT_PORT = 3000;
 
-const port = parseInt(Deno.env.get('PORT') ?? '8000');
+const port = parseInt(Deno.env.get('PORT') ?? 'DEFAULT_PORT' );
 const s = serve({ port });
 
 const app = opine();
@@ -27,107 +27,107 @@ for await (const req of s) {
     headers: new Headers({ 'Content-Type': 'text/html' }),
   });
 
-  app.get("/trailid/:trailid", async function ({
-    req,
-    res,
-  }: {
-    req: { id: string };
-    res: any;
-  }): Promise<void> {
+  // app.get("/trailid/:trailid", async function ({
+  //   req,
+  //   res,
+  // }: {
+  //   req: { id: string };
+  //   res: any;
+  // }): Promise<void> {
 
-    console.log("Das ist req", req);
-    console.log("Das ist req.id", req.id);
-    const trail = await trails.find({ "_id": new Bson.ObjectID(req.id) }, { noCursorTimeout: false }).toArray();
+  //   console.log("Das ist req", req);
+  //   console.log("Das ist req.id", req.id);
+  //   const trail = await trails.find({ "_id": new Bson.ObjectID(req.id) }, { noCursorTimeout: false }).toArray();
 
-    if (trail) {
-      res.status = 200;
-      res.body = {
-        success: true,
-        data: trail,
-      };
-    } else {
-      res.status = 404;
-      res.body = {
-        success: false,
-        msg: "No trail found",
-      };
-    }
-  });
+  //   if (trail) {
+  //     res.status = 200;
+  //     res.body = {
+  //       success: true,
+  //       data: trail,
+  //     };
+  //   } else {
+  //     res.status = 404;
+  //     res.body = {
+  //       success: false,
+  //       msg: "No trail found",
+  //     };
+  //   }
+  // });
 
-  app.get("/alltrails", async function (req, res) {
-    res.send(await trails.find({}, { noCursorTimeout: false }).toArray())
-  });
+  // app.get("/alltrails", async function (req, res) {
+  //   res.send(await trails.find({}, { noCursorTimeout: false }).toArray())
+  // });
 
-  app.get("/search", async function (req, res) {
-    const trailService = new TrailService();
-    let searchParameters = trailService.findACoolTrail({
-      trailId: req.query.trailid,
-      length: req.query.length,
-      title: req.query.title,
-      rating: req.query.rating,
-      coordinates: req.query.coordinates
-    });
-    let searchQuery = JSON.stringify(searchParameters);
-    console.log(searchQuery);
-    res.send(await trails.find({ searchQuery }, { noCursorTimeout: false }));
-    /* result 
-    correct string is passed, but is not correctly formatted for search in Mongo
-    */
-  });
+  // app.get("/search", async function (req, res) {
+  //   const trailService = new TrailService();
+  //   let searchParameters = trailService.findACoolTrail({
+  //     trailId: req.query.trailid,
+  //     length: req.query.length,
+  //     title: req.query.title,
+  //     rating: req.query.rating,
+  //     coordinates: req.query.coordinates
+  //   });
+  //   let searchQuery = JSON.stringify(searchParameters);
+  //   console.log(searchQuery);
+  //   res.send(await trails.find({ searchQuery }, { noCursorTimeout: false }));
+  //   /* result 
+  //   correct string is passed, but is not correctly formatted for search in Mongo
+  //   */
+  // });
 
-  app.set("/addTrail", async ({
-    req, res,
-  }: {
-    req: any; res: any;
-  }): Promise<void> => {
-    try {
-      if (!req.hasBody) {
-        res.status = 400;
-        res.body = {
-          success: false,
-          msg: "No Data",
-        };
-      } else {
-        const body = await req.body();
-        const trail = await body.value;
-        await trails.insertOne(trail);
-        res.status = 201;
-        res.body = {
-          success: true,
-          data: trail,
-        };
-      }
-    } catch (err) {
-      res.body = {
-        success: false,
-        msg: err.toString(),
-      };
-    }
-  });
+  // app.set("/addTrail", async ({
+  //   req, res,
+  // }: {
+  //   req: any; res: any;
+  // }): Promise<void> => {
+  //   try {
+  //     if (!req.hasBody) {
+  //       res.status = 400;
+  //       res.body = {
+  //         success: false,
+  //         msg: "No Data",
+  //       };
+  //     } else {
+  //       const body = await req.body();
+  //       const trail = await body.value;
+  //       await trails.insertOne(trail);
+  //       res.status = 201;
+  //       res.body = {
+  //         success: true,
+  //         data: trail,
+  //       };
+  //     }
+  //   } catch (err) {
+  //     res.body = {
+  //       success: false,
+  //       msg: err.toString(),
+  //     };
+  //   }
+  // });
 
-  app.delete("/deleteTrail", async function ({
-    req, res
-  }: {
-    req: { id: string; }; res: any;
-  }): Promise<void> {
-    try {
-      await trails.deleteOne({ $oid: req.id });
-      res.status = 201;
-      res.body = {
-        success: true,
-        msg: "Trail deleted",
-      };
-    } catch (err) {
-      res.body = {
-        success: false,
-        msg: err.toString(),
-      };
-    }
-  });
+  // app.delete("/deleteTrail", async function ({
+  //   req, res
+  // }: {
+  //   req: { id: string; }; res: any;
+  // }): Promise<void> {
+  //   try {
+  //     await trails.deleteOne({ $oid: req.id });
+  //     res.status = 201;
+  //     res.body = {
+  //       success: true,
+  //       msg: "Trail deleted",
+  //     };
+  //   } catch (err) {
+  //     res.body = {
+  //       success: false,
+  //       msg: err.toString(),
+  //     };
+  //   }
+  // });
 
-  app.listen(
-    { port: port ?? DEFAULT_PORT },
-    () => console.log(`Server has started on http://localhost:${port} 🚀`),
-  );
+  // app.listen(
+  //   { port: port ?? DEFAULT_PORT },
+  //   () => console.log(`Server has started on http://localhost:${port} 🚀`),
+  // );
 }
 
