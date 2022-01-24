5f66a6fc02b7bc39aff5b94a907c5bb2fd9c549d
maximilian-graf2019 <maxgraf27@gmail.com>
2022-01-24 11:19:09 +0100

    get Port dynamically

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
-const PORT = 4000;
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno;
+const DEFAULT_PORT = 8000;
+const PORT = parse(args).port;
+//const PORT : number  = +Deno.env.get("PORT");
 
 const app = new Application();
 
